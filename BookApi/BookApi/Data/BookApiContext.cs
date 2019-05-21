f54e3b6ce7f8ef3534c7400f28fb2ffb3788ea2b
jazytestko <49674564+jazytestko@users.noreply.github.com>
2019-05-21 03:27:00 +0000

    Add files via upload

---
+++ BookApi/BookApi/Data/BookApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BooksApi.Models;
+
+namespace BookApi.Models
+{
+    public class BookApiContext : DbContext
+    {
+        public BookApiContext (DbContextOptions<BookApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BooksApi.Models.Book> Book { get; set; }
+    }
+}
